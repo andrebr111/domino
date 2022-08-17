9a77868984209e5a1d41c63d52687ab91087295e
andre <105241564+andrebr111@users.noreply.github.com>
2022-08-16 21:53:13 -0300

    Update DOM_BGR_View.h

--- DOM_BGR_View.h
+++ DOM_BGR_View.h
@@ -1,8 +1,10 @@
 //DOM_BGR_View.h 12/08/22
 //Andre Cavacanti, Marcello Linard, Thiago amaral
 
+#include"Dom_BGR_Model.cpp"
 #include <stdio.h>
 #include <stdlib.h>
 #include<time.h>
 
-int fMenuGeral();
+int fMenuGeral(int opc);
+void fApresentar(struct Pc Pec[28]);
