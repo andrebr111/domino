122987b83d7fc67f4a2b6e8e44317354b76514c8
andre <105241564+andrebr111@users.noreply.github.com>
2022-08-23 19:24:09 -0300

    Update DOM_BGR_Controller.cpp

--- DOM_BGR_Controller.cpp
+++ DOM_BGR_Controller.cpp
@@ -2,10 +2,6 @@
 //Andre Cavacanti, Marcello Linard, Thiago amaral
 
 #include"DOM_BGR_Controller.h"
-#include"DOM_B//DOM_BGR_Controller 12/08/22
-//Andre Cavacanti, Marcello Linard, Thiago amaral
-
-#include"DOM_BGR_Controller.h"
 #include"DOM_BGR_View.cpp"
 #include <stdio.h>
 #include <stdlib.h>
@@ -139,56 +135,4 @@ void fIniciarjogo(struct Pc Pec[28])
 
 	printf("MESA: [%d|%d] ", Pec[u].lado1, Pec[u].lado2);
 	printf("MESA: [%d|%d] ", Mesa[0].lado1, Mesa[0].lado2);
-}GR_View.cpp"
-#include <stdio.h>
-#include <stdlib.h>
-#include<time.h>
-
-//gera as pecas do domino
-void fGerarPecas(struct Pc Pec[28]) 
-{
-	int i,j, count = 0;
-	for(i = 0; i <= 6; i++)
-		for(j = i; j <= 6; j++)
-		{
-			Pec[count].lado1 = i;
-			Pec[count].lado2 = j;
-			count++;
-		}	
-}
-
-//embaralha as pecas do domino
-void fEmbaralharPecas(struct Pc Pec[28])
-{
-	srand(time(0));
-	int i,j;
-	int random = 0;
-	struct Pc aux ;
-	
-	for(i = 0; i < 28; i++)
-	{
-		random = rand() % 28;
-		aux = Pec[i] ;
-		Pec[i] = Pec[random];
-		Pec[random] = aux;
-	}
-}
-
-//inicializa o menu
-void fInicializarMenu(int oper, struct Pc Pec[28])
-{
-	int opr;
-	
-	do
-	{
-		opr = fMenuGeral(opr);
-		switch (opr)
-		{
-			case 1:
-				fApresentar(Pec);
-				break;
-			case 2:	
-				break; 
-			}
-	}while( opr != 2);
 }
