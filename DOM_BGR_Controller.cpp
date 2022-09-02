5e99c25f178f927594611c61b274543755db4120
andre <105241564+andrebr111@users.noreply.github.com>
2022-09-02 01:25:29 -0300

    Update DOM_BGR_Controller.cpp

--- DOM_BGR_Controller.cpp
+++ DOM_BGR_Controller.cpp
@@ -61,7 +61,7 @@ void fInicializarMenu(int oper, struct Pc Pec[28])
 			case 6:	
 				break;
 			case 0:
-				break;					 
+				break;						 
 			}
 	}while( opr != 0);
 }
@@ -70,13 +70,17 @@ void fInicializarMenu(int oper, struct Pc Pec[28])
 void fIniciarjogo(struct Pc Pec[28])
 {
 	int i;
-	char opr;
 	
 	fGerarPecas(Pec);
 	
 	for(i = 0; i < 28; i++)
 	{
 		Pec[i].status =' ';
+	}
+	
+	for(i = 0; i < 28; i++)
+	{
+		Mesa[i].status = ' ';
 	}
 	
 	fEmbaralharPecas(Pec);
@@ -96,13 +100,19 @@ void fIniciarjogo(struct Pc Pec[28])
 //seleciona a primeira peca que vai na mesa 
 void fprimeirapeca(struct Pc Pec[28])
 {
-	int i, u;
-	char z;
-	char opr;
+	int i, k, u, x, loc, mesap, fim, cont, cont2, compr, N, lado, opr, comp;
+	char z, test;
+	int Tmesa, empate;
+	int ladD, ladE;
 	
 	aux.status = ' ';
 	aux.lado1 = 0; 
 	aux.lado2 = 1;
+	mesap=1;
+	compr=13;
+	lado=0;
+    empate=0;
+    Tmesa=5;
 	
 	for(i=0; i<=13; i++)
 	{
@@ -127,47 +137,407 @@ void fprimeirapeca(struct Pc Pec[28])
 			}
 		}	
 	}
+	Tmesa = Tmesa+7;
+	
+	fprimeiro(Pec,u);
+	
 	Mesa[0].lado1 = aux.lado1;
 	Mesa[0].lado2 = aux.lado2;
-	
-	fMesa(Pec,u);
+	ladD = Mesa[0].lado2;
+	ladE = Mesa[0].lado1;
+
 	
 	if(Pec[u].status=='1')
 	{
-    z='2';
+    	z='2';
     } 
-    else
+    else if(Pec[u].status=='2')
     {
-    z='1';
+    	z='1';
     }
-    Pec[u].status = 'm';
-    fApresentar(Pec, u, z);
-    fmenujogar(Pec, opr);
-}
-
-//inicia as opcoes do jogador
-void fmenujogar(struct Pc Pec[28], char oper)
-{
-	char opr;
-	int u;
+    
+	Pec[u].status = 'm';
 	do
 	{
-		char opr;
-		opr = fjogar(Pec, u, opr);
-		switch (opr)
+		i = 0;
+		do
 		{
-			case 'J':
-			case 'j':	
+			fMesa(Mesa, i);
+			i++;
+		}while(i<mesap);
+		
+	    fApresentar(Pec, z);
+	    
+	    //inicia as opcoes do jogador
+	    
+	    opr = fjogar(opr, ladD, ladE);
+	    
+	    cont = 0;
+	    
+	    switch(opr)
+	    {
+	    	case 1:
+	    		loc = fescpec(loc);
+	    		
+	    		if(loc==0)
+	           	{
+	                break;
+	            }
+	            
+	            for(i=0; i<27; i++)
+	            {
+	            	if(Pec[i].status==z)
+					{
+						cont++;
+			    	}
+				    if(cont==loc)
+				    {
+				    	if(Pec[i].lado1 == ladE || Pec[i].lado1 ==ladD || Pec[i].lado2 == ladE || Pec[i].lado2 == ladD)
+				    	{
+				    		empate = 0;
+				    		if(((Pec[i].lado1 == ladE ||  Pec[i].lado1 ==ladD ) && (Pec[i].lado2==ladE || Pec[i].lado2==ladD)) || ladE == ladD)
+				    		{
+				    			lado = fladmesa(lado);
+				    			
+				    			if( lado=='E' || lado=='e')
+				    			{
+				    				if(Pec[i].lado1 == ladE || Pec[i].lado2 == ladE)
+				    				{
+				    					for(N=mesap;N>=0;N--)
+								   		{
+				                    		Mesa[N].lado1 = Mesa[N-1].lado1;  
+				                    		Mesa[N].lado2 = Mesa[N-1].lado2; 
+							        	}
+							        	if(Pec[i].lado1==ladE)
+									    {
+									    	Mesa[0].lado1 = Pec[i].lado2 ;
+									    	Mesa[0].lado2 = Pec[i].lado1;
+									    	mesap++;
+									    	ladE=Mesa[0].lado1;
+									    	Pec[i].status = 'm';
+									    	if(z=='1')
+	                                        {
+	                                            z='2';
+	                                        }
+	                                        else if(z == '2')
+	                                        {
+	                                            z='1';
+	                                        }
+	                                        Tmesa = Tmesa+6;
+									    	break;
+									    }	
+									    else if(Pec[i].lado2==ladE)
+									    {
+									    	Mesa[0].lado1 = Pec[i].lado1;
+									    	Mesa[0].lado2 = Pec[i].lado2;
+									    	mesap++;
+									    	ladE=Mesa[0].lado1;
+									    	Pec[i].status = 'm';
+									    	if(z=='1')
+	                                        {
+	                                            z='2';
+	                                        }
+	                                        else if(z == '2')
+	                                        {
+	                                            z='1';
+	                                        }
+	                                        Tmesa = Tmesa+6;
+							            	break;
+									    }
+									}
+									else
+									{
+										ladoErrado();
+									}
+								}
+								else if(lado=='D' || lado=='d')
+								{
+									if(Pec[i].lado1 == ladD || Pec[i].lado2 == ladD)
+									{
+										if(Pec[i].lado1 == ladD)
+										{
+										    Mesa[mesap].lado1=Pec[i].lado1;
+										    Mesa[mesap].lado2=Pec[i].lado2;
+								            ladD=Mesa[mesap].lado2;
+								            mesap++;
+								            Pec[i].status = 'm';
+								            if(z=='1')
+	                                        {
+	                                            z='2';
+	                                        }
+	                                        else if(z == '2')
+	                                        {
+	                                            z='1';
+	                                        }
+	                                        Tmesa = Tmesa+6;
+								        	break;
+								        }
+							            else if(Pec[i].lado2 == ladD)
+									    {
+										    Mesa[mesap].lado1=Pec[i].lado2;
+										    Mesa[mesap].lado2=Pec[i].lado1;
+								            ladD=Mesa[mesap].lado2;
+								            mesap++;
+								            Pec[i].status = 'm';
+								            if(z=='1')
+	                                        {
+	                                            z='2';
+	                                        }
+	                                        else if(z == '2')
+	                                        {
+	                                            z='1';
+	                                        }
+	                                        Tmesa = Tmesa+6;
+								        	break;
+										}
+									}
+								}
+								else
+								{
+									ladoErrado();
+								}  
+							}
+							else
+							{
+								if(Pec[i].lado1==ladE)
+	                            {
+	                                for(N=mesap; N>=0; N--)
+	                                {
+	                                    Mesa[N].lado1 = Mesa[N-1].lado1;
+	                                    Mesa[N].lado2 = Mesa[N-1].lado2;
+	                                }
+	                                Mesa[0].lado1 = Pec[i].lado2 ;
+	                                Mesa[0].lado2 = Pec[i].lado1;
+	                                mesap++;
+	                                ladE=Mesa[0].lado1;
+	                                Pec[i].status = 'm';
+	                                if(z=='1')
+	                                {
+	                                    z='2';
+	                                }
+	                                else if(z == '2')
+	                                {
+	                                    z='1';
+	                                }
+	                                Tmesa = Tmesa+6;
+	                                break;
+	                            }
+	                            else if(Pec[i].lado2==ladE)
+	                            {
+	                                for(N=mesap; N>=0; N--)
+	                                {
+	                                    Mesa[N].lado1 = Mesa[N-1].lado1;
+	                                    Mesa[N].lado2 = Mesa[N-1].lado2;
+	                                }
+	                                Mesa[0].lado1 = Pec[i].lado1;
+	                                Mesa[0].lado2 = Pec[i].lado2;
+	                                mesap++;
+	                                ladE=Mesa[0].lado1;
+	                                Pec[i].status = 'm';
+	                                if(z=='1')
+	                                {
+	                                    z='2';
+	                                }
+	                                else if(z == '2')
+	                                {
+	                                    z='1';
+	                                }
+	                                Tmesa = Tmesa+6;
+	                                break;
+	                            }
+	                            else if(Pec[i].lado1 == ladD)
+	                            {
+	                                Mesa[mesap].lado1=Pec[i].lado1;
+	                                Mesa[mesap].lado2=Pec[i].lado2;
+	                                ladD=Mesa[mesap].lado2;
+	                                mesap++;
+	                                Pec[i].status = 'm';
+	                                if(z=='1')
+	                                {
+	                                    z='2';
+	                                }
+	                                else if(z == '2')
+	                                {
+	                                    z='1';
+	                                }
+	                                Tmesa = Tmesa+6;
+	                                break;
+	                            }
+	                            else if(Pec[i].lado2 == ladD)
+	                            {
+	                                Mesa[mesap].lado1=Pec[i].lado2;
+	                                Mesa[mesap].lado2=Pec[i].lado1;
+	                                ladD=Mesa[mesap].lado2;
+	                                mesap++;
+	                                Pec[i].status = 'm';
+	                                if(z=='1')
+	                                {
+	                                    z='2';
+	                                }
+	                                else if(z == '2')
+	                                {
+	                                    z='1';
+	                                }
+	                                Tmesa = Tmesa+6;
+	                                break;
+	                            }
+							}
+						}
+						else
+						{
+							fnumeracaoErrada();
+						}
+					}
+				}
 				break;
-			case 'C':
-			case 'c':	
+			case 2:
+				if(comp==27)
+				{
+					fmsgcomp();
+				}
+				else
+				{
+					comp++;
+					Pec[comp].status=z;	
+				}
 				break;
-			case 'P':
-			case 'p':
+			case 3:
+				cont=0;
+           		if(comp==26 )
+				{
+					for(i=0; i<27; i++)
+					{
+						if(Pec[i].status == z)
+                    	{
+                        	if(Pec[i].lado1 == ladE || Pec[i].lado1 ==ladD || Pec[i].lado2 == ladE || Pec[i].lado2 == ladD)
+                        	{
+                            	cont++;
+                        	}
+                    	}	
+					}
+					if( cont == 0)
+	                {
+	                    empate++;
+	                    if(z=='1')
+	                    {
+	                        z='2';
+	                    }
+	                    else if(z == '2')
+	                    {
+	                        z='1';
+	                    }
+	                }
+	                else
+	                {
+	                	fcontinuajogo();
+					}
+				}
+				else
+				{
+					fcontinuajogo();
+				}
 				break;
-			case 'S':
-			case 's':
-				break;				 
+			case 0:
+				fim=3;
+
+            	break;
+
+        	default:
+				fmsgopinvalida();
+            	break;			
 		}
-	}while( opr != 'S' && opr != 's');
+		cont=0;
+        cont2=0;
+
+        if(empate == 2)
+        {
+        	for(i=0; i<27; i++)
+            {
+				if (Pec[i].status=='1')
+                {
+                    cont++;
+                }
+                if(Pec[i].status=='2')
+                {
+                    cont2++;
+                }
+            }
+            if(cont<cont2)
+            {
+                fim=1;
+                fsemjoagadas();
+                fcontagem();
+            }
+            else if(cont2<cont)
+            {
+                fim=2;
+                fsemjoagadas();
+                fcontagem();
+            }
+            else if(cont==cont2)
+            {
+                cont=0;
+                cont2=0;
+
+                for(i=0; i<27; i++)
+                {
+                    if(Pec[i].status=='1')
+                    {
+                        cont = cont + Pec[i].lado1;
+                        cont = cont + Pec[i].lado2;
+                    }
+                    else if(Pec[i].status=='2')
+                    {
+                        cont2 = cont2 + Pec[i].lado1;
+                        cont2 = cont2 + Pec[i].lado2;
+                    }
+                }
+                if(cont<cont2)
+                {
+                    fim=1;
+                    fsemjoagadas();
+                	fcontagem();
+                }
+                if(cont2<cont)
+                {
+                    fim=2;
+                    fsemjoagadas();
+               		fcontagem();
+                }
+                if(cont==cont2)
+                {
+                    fim=4;
+                    fsemjoagadas();
+                	fcontagem();
+                }
+
+            }
+		}
+		cont=0;
+        cont2=0;
+        for(i=0; i<28; i++)
+        {
+        	if (Pec[i].status =='1')
+            {
+                cont++;
+            }
+            if(Pec[i].status == '2')
+            {
+                cont2++;
+            }
+		}
+		if(cont==0 && comp==26)
+        {
+            fim=1;
+        }
+        if(cont2==0 && comp==26)
+        {
+            fim=2;
+        }
+	    
+	   
+	}while (fim == 0 );
+	
+	fmsgfimjogo(fim);
+	
 }
