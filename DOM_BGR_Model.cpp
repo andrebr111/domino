e052ba7bfc7f1bc83c5e02d4ae79fa1360d5ff23
andre <105241564+andrebr111@users.noreply.github.com>
2022-09-27 16:14:34 -0300

    Update DOM_BGR_Model.cpp

--- DOM_BGR_Model.cpp
+++ DOM_BGR_Model.cpp
@@ -7,19 +7,31 @@
 #ifndef Model_H_
 #define Model_H_
 
-
+//estrutura das pecas do jogo
 struct Pc
 {
 	int lado1;
 	int lado2;
 	char status;
+	
 } Pec[28], aux;
 
+//estrutura da mesa no jogo
 struct Tmesa
 {
 	int lado1;
 	int lado2;
 	char status;
+	
 }Mesa[28];
 
+int mesap; //contagem de pecas na mesa 
+char z;	//vez do jogador 
+int comp;	//pecas compradas 
+int emp; //caso tenha um empate 
+int bot; //separar quando o jogo e iniciado com bot ou 2 jogadores
+int ladD; //extremo direito da mesa 
+int ladE; //exttremo esquerdo da mess
+
 #endif 
+
